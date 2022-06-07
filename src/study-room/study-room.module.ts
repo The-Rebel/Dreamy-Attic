15108ebca771f4e8cb5f4eab589b1ac380799c66
jeongjiwoo0522 <jiwoourty@gmail.com>
2022-06-07 10:47:15 +0900

    #1 create study-room module

---
+++ src/study-room/study-room.module.ts
@@ -0,0 +1,23 @@
+import { Module } from "@nestjs/common";
+import { StudyRoomMemoryAdaptor } from "@src/study-room/adaptor/outbound/memory/study-room-memory.adaptor";
+import { StudyRoomService } from "@src/study-room/application/service/study-room.service";
+import { SaveStudyRoomPortToken } from "@src/study-room/application/port/outbound/save-study-room.port";
+import { CreateStudyRoomUseCaseToken } from "@src/study-room/application/port/inbound/create-study-room.usecase";
+import { StudyRoomController } from "@src/study-room/adaptor/inbound/web/study-room.controller";
+
+@Module({
+    controllers: [StudyRoomController],
+    providers: [
+        StudyRoomMemoryAdaptor,
+        StudyRoomService,
+        {
+            provide: SaveStudyRoomPortToken,
+            useClass: StudyRoomMemoryAdaptor
+        },
+        {
+            provide: CreateStudyRoomUseCaseToken,
+            useClass: StudyRoomService
+        }
+    ]
+})
+export class StudyRoomModule {}
