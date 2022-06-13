878d3c1f3dd3496f57d0a47c81bc9208538ae31f
jeongjiwoo0522 <jiwoourty@gmail.com>
2022-06-13 23:42:18 +0900

    use jwt auth guard

--- src/study-room/adaptor/inbound/web/study-room.controller.ts
+++ src/study-room/adaptor/inbound/web/study-room.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Inject, Post } from "@nestjs/common";
+import { Body, Controller, Inject, Post, UseGuards } from "@nestjs/common";
 import { CreateStudyRoomUseCase, CreateStudyRoomUseCaseToken } from "@src/study-room/application/port/inbound/create-study-room.usecase";
 import { CreateStudyRoomRequest } from "@src/study-room/application/port/inbound/dto/request/create-study-room.request";
+import { AuthGuard } from "@nestjs/passport";
 
 @Controller("study-room")
 export class StudyRoomController {
@@ -9,6 +10,7 @@ export class StudyRoomController {
         private readonly createStudyRoomUseCase: CreateStudyRoomUseCase
     ) {}
 
+    @UseGuards(AuthGuard("jwt"))
     @Post()
     async studyRoom(@Body() request: CreateStudyRoomRequest) {
         await this.createStudyRoomUseCase.createStudyRoom(request);
