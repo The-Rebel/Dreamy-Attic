c5d3cf95b0036429c4a90b7f3d05e8dad8052c58
jeongjiwoo0522 <jiwoourty@gmail.com>
2022-06-13 22:36:13 +0900

    #9 import config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from "@nestjs/common";
 import { StudyRoomModule } from "@src/study-room/study-room.module";
 import { UserModule } from "@src/user/user.module";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-    imports: [StudyRoomModule, UserModule]
+    imports: [
+        StudyRoomModule,
+        UserModule,
+        ConfigModule.forRoot({
+            isGlobal: true
+        })
+    ]
 })
 export class AppModule {}
