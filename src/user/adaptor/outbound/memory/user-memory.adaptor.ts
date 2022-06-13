5c1ca67696d86260b38c0fff480a7185b1280833
jeongjiwoo0522 <jiwoourty@gmail.com>
2022-06-13 22:40:10 +0900

    #9 implement find by username port

--- src/user/adaptor/outbound/memory/user-memory.adaptor.ts
+++ src/user/adaptor/outbound/memory/user-memory.adaptor.ts
@@ -1,15 +1,23 @@
 import { SaveUserPort } from "@src/user/application/port/outbound/save-user.port";
 import { User } from "@src/user/domain/user";
 import { Injectable } from "@nestjs/common";
+import { FindByUserNamePort } from "@src/user/application/port/outbound/find-by-username.port";
 
 @Injectable()
-export class UserMemoryAdaptor implements SaveUserPort {
-    private users: User[] = [];
-    private lastIndex: number = 0;
+export class UserMemoryAdaptor implements SaveUserPort, FindByUserNamePort {
+    private static readonly users: User[] = [];
+    private static lastIndex: number = 0;
 
     async saveUser(user: User): Promise<void> {
-        user.id = ++this.lastIndex;
-        this.users.push(user);
-        console.log(this.users);
+        user.id = ++UserMemoryAdaptor.lastIndex;
+        UserMemoryAdaptor.users.push(user);
+    }
+
+    async findByUserName(username: string): Promise<User> {
+        const user: User = UserMemoryAdaptor.users.filter(u => u.username === username)[0];
+        if (!user) {
+            throw new Error("user not found");
+        }
+        return user;
     }
 }
