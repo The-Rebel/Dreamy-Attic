4997e5d734a4d15821dd58ca507d6c30ad619526
jeongjiwoo0522 <jiwoourty@gmail.com>
2022-06-13 22:42:09 +0900

    #9 add user sign in provider

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,8 +4,13 @@ import { UserMemoryAdaptor } from "@src/user/adaptor/outbound/memory/user-memory
 import { UserService } from "@src/user/application/service/user.service";
 import { SignUpUserUseCaseToken } from "@src/user/application/port/inbound/signup-user.usecase";
 import { SaveUserPortToken } from "@src/user/application/port/outbound/save-user.port";
+import { JwtModule } from "@nestjs/jwt";
+import { JwtStrategy } from "@src/global/passport-jwt/jwt.strategy";
+import { SignInUserUseCaseToken } from "@src/user/application/port/inbound/signin-user.usecase";
+import { FindByUserNamePortToken } from "@src/user/application/port/outbound/find-by-username.port";
 
 @Module({
+    imports: [JwtModule.register({})],
     controllers: [UserController],
     providers: [
         UserMemoryAdaptor,
@@ -17,7 +22,16 @@ import { SaveUserPortToken } from "@src/user/application/port/outbound/save-user
         {
             provide: SignUpUserUseCaseToken,
             useClass: UserService
-        }
+        },
+        {
+            provide: FindByUserNamePortToken,
+            useClass: UserMemoryAdaptor
+        },
+        {
+            provide: SignInUserUseCaseToken,
+            useClass: UserService
+        },
+        JwtStrategy
     ]
 })
 export class UserModule {}
