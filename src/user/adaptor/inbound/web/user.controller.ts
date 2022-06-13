b168ffb731b3b0d0b9bb390a6228bebe8b87a642
jeongjiwoo0522 <jiwoourty@gmail.com>
2022-06-13 22:41:33 +0900

    #9 sign in user web adaptor

--- src/user/adaptor/inbound/web/user.controller.ts
+++ src/user/adaptor/inbound/web/user.controller.ts
@@ -1,16 +1,26 @@
 import { Body, Controller, Inject, Post } from "@nestjs/common";
 import { SignUpUserUseCase, SignUpUserUseCaseToken } from "@src/user/application/port/inbound/signup-user.usecase";
 import { SignUpUserRequest } from "@src/user/application/port/inbound/dto/request/signup-user.request";
+import { SignInUserUseCase, SignInUserUseCaseToken } from "@src/user/application/port/inbound/signin-user.usecase";
+import { SignInUserRequest } from "@src/user/application/port/inbound/dto/request/signin-user.request";
 
 @Controller("/user")
 export class UserController {
     constructor(
         @Inject(SignUpUserUseCaseToken)
-        private readonly signUpUserUseCase: SignUpUserUseCase
+        private readonly signUpUserUseCase: SignUpUserUseCase,
+
+        @Inject(SignInUserUseCaseToken)
+        private readonly signInUserUseCase: SignInUserUseCase
     ) {}
 
     @Post()
-    async user(@Body() request: SignUpUserRequest) {
-        await this.signUpUserUseCase.signUpUser(request);
+    public user(@Body() request: SignUpUserRequest) {
+        return this.signUpUserUseCase.signUpUser(request);
+    }
+
+    @Post("/token")
+    public userToken(@Body() request: SignInUserRequest) {
+        return this.signInUserUseCase.signInUser(request);
     }
 }
