54b836ee712732dce1b77f7e8cdece646b2efa23
jeongjiwoo0522 <jiwoourty@gmail.com>
2022-06-13 11:10:25 +0900

    #7 refactoring study room adaptor
    private property

--- src/study-room/adaptor/outbound/memory/study-room-memory.adaptor.ts
+++ src/study-room/adaptor/outbound/memory/study-room-memory.adaptor.ts
@@ -4,12 +4,11 @@ import { StudyRoom } from "@src/study-room/domain/study-room";
 
 @Injectable()
 export class StudyRoomMemoryAdaptor implements SaveStudyRoomPort {
-    studyRooms: StudyRoom[] = [];
-    lastIndex: number = 0;
+    private studyRooms: StudyRoom[] = [];
+    private lastIndex: number = 0;
 
     async saveStudyRoom(studyRoom: StudyRoom): Promise<void> {
         studyRoom.id = ++this.lastIndex;
         this.studyRooms.push(studyRoom);
-        console.log(this.studyRooms);
     }
 }
