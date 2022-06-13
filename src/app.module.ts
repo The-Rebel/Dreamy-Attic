03902861a4c4510cb370b66046667bdfddfa377a
jeongjiwoo0522 <jiwoourty@gmail.com>
2022-06-13 11:34:58 +0900

    #7 create user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from "@nestjs/common";
 import { StudyRoomModule } from "@src/study-room/study-room.module";
+import { UserModule } from "@src/user/user.module";
 
 @Module({
-    imports: [StudyRoomModule]
+    imports: [StudyRoomModule, UserModule]
 })
 export class AppModule {}
