1e16c1002c8cfecbb74d124ae8058f8b1928a21b
정지우 <jiwoourty@gmail.com>
2022-07-25 11:33:39 +0900

    :bug: :: get /study-room routing

--- src/study-room/adaptor/inbound/web/study-room.controller.ts
+++ src/study-room/adaptor/inbound/web/study-room.controller.ts
@@ -1,18 +1,32 @@
-import { Body, Controller, Inject, Post, UseGuards } from "@nestjs/common";
+import { Body, Controller, Get, Inject, Post, UseGuards } from "@nestjs/common";
 import { CreateStudyRoomUseCase, CreateStudyRoomUseCaseToken } from "@src/study-room/application/port/inbound/create-study-room.usecase";
 import { CreateStudyRoomRequest } from "@src/study-room/application/port/inbound/dto/request/create-study-room.request";
 import { AuthGuard } from "@nestjs/passport";
+import {
+    GetStudyRoomListUseCase,
+    GetStudyRoomListUseCaseToken
+} from "@src/study-room/application/port/inbound/get-study-room-list.usecase";
+import { GetStudyRoomListResponse } from "@src/study-room/application/port/inbound/dto/response/get-study-room-list.response";
 
 @Controller("study-room")
 export class StudyRoomController {
     constructor(
         @Inject(CreateStudyRoomUseCaseToken)
-        private readonly createStudyRoomUseCase: CreateStudyRoomUseCase
+        private readonly createStudyRoomUseCase: CreateStudyRoomUseCase,
+
+        @Inject(GetStudyRoomListUseCaseToken)
+        private readonly getStudyRoomListUseCase: GetStudyRoomListUseCase
     ) {}
 
     @UseGuards(AuthGuard("jwt"))
     @Post()
     async studyRoom(@Body() request: CreateStudyRoomRequest): Promise<void> {
         await this.createStudyRoomUseCase.createStudyRoom(request);
+    }
+
+    @UseGuards(AuthGuard("jwt"))
+    @Get()
+    studyRoomList(): Promise<GetStudyRoomListResponse> {
+        return this.getStudyRoomListUseCase.getStudyRoomList();
     }
 }
